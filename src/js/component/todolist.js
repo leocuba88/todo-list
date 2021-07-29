9b0226a3e7b919c3d81b97b2576bdc4cf241eb1f
leocuba88 <leocuba88@hotmail.com>
2021-07-29 23:00:01 +0000

    doing

--- src/js/component/todolist.js
+++ src/js/component/todolist.js
@@ -8,6 +8,10 @@ export function TodoList() {
 		"Practice piano"
 	]);
 
+	// React.useEffect(() => {
+	// 	console.log("An item was add: ", userInput);
+	// }, [userInput]);
+
 	const handleKeyPress = event => {
 		if (event.key === "Enter") {
 			let newArr = [...todolist];
@@ -27,31 +31,31 @@ export function TodoList() {
 	return (
 		<div className="body">
 			<h1>todos</h1>
-			<input
-				type="text"
-				id="first_name"
-				value={userInput}
-				onKeyDown={handleKeyPress}
-				onChange={event => {
-					setUserInput(event.target.value);
-				}}></input>
-			<ul>
-				{todolist.map((task, i) => {
-					return (
-						<div className="buttonAppear">
-							<li className="item" key={i}>
-								{task}{" "}
-								<button
-									className="textButton"
-									value={i}
-									onClick={deleteTodo}>
-									X
-								</button>
-							</li>
-						</div>
-					);
-				})}
-			</ul>
+			<div className="boxShadow">
+				<input
+					type="text"
+					id="first_name"
+					value={userInput}
+					onKeyDown={handleKeyPress}
+					onChange={event => {
+						setUserInput(event.target.value);
+					}}></input>
+				<ul>
+					{todolist.map((task, i) => {
+						return (
+							<div className="buttonAppear">
+								<li className="item" key={i}>
+									{task}{" "}
+									<button value={i} onClick={deleteTodo}>
+										X
+									</button>
+								</li>
+							</div>
+						);
+					})}
+					<li className="itemsLeft">{todolist.length} items left</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
